68a520e5c8f055329ab2596860a1e29c81974a47
yuna gando <gando@grean.tech>
2022-12-14 18:43:29 +0900

    エラー解消

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
+import Layout from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
 
 export async function getStaticProps() {
     const allPostsData = getSortedPostsData();
@@ -14,9 +16,9 @@ export async function getStaticProps() {
 export default function Home({ allPostsData }) {
     return (
         <Layout home>
-            {/* Keep the existing code here */}
+            {/* 既存のコードをここに残す */}
 
-            {/* Add this <section> tag below the existing <section> tag */}
+            {/* 既存の<section>タグの下に、次の<section>タグを追加する */}
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
                 <h2 className={utilStyles.headingLg}>Blog</h2>
                 <ul className={utilStyles.list}>
