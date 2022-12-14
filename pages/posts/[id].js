23665f8301de2c02e687b2baa1d03977839a89fc
yuna gando <gando@grean.tech>
2022-12-14 15:48:51 +0900

    エラーが出ていたところを修正。Dynamic Routesの4まで完了

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,69 +1,23 @@
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
-import Head from 'next/head';
-import Date from '../../components/date';
-import utilStyles from '../../styles/utils.module.css';
 
-export default function Post({ postData }) {
-  return (
-    <Layout>
-      <Head>
-        <title>{postData.title}</title>
-      </Head>
-      <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      </article>
-    </Layout>
-  );
-}
-
-export default function Post({ postData }) {
-  return (
-    <Layout>
-      {/* Keep the existing code here */}
-
-      {/* Replace {postData.date} with this */}
-      <Date dateString={postData.date} />
-
-      {/* Keep the existing code here */}
-    </Layout>
-  );
-}
-
-// export default function Post({ postData }) {
-//   return (
-//     <Layout>
-//       {/* Add this <Head> tag */}
-//       <Head>
-//         <title>{postData.title}</title>
-//       </Head>
-
-//       {/* Keep the existing code here */}
-//     </Layout>
-//   );
-// }
-
+// 静的なプロパティを取得する非同期関数
 export async function getStaticProps({ params }) {
-    // Add the "await" keyword like this:
-    const postData = await getPostData(params.id);
-  
-    return {
-      props: {
-        postData,
-      },
-    };
-  }
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
 
+// 静的なパスを取得する非同期関数
 export async function getStaticPaths() {
-    const paths = getAllPostIds();
-    return {
-        paths,
-        fallback: false,
-    };
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
 export default function Post({ postData }) {
@@ -74,21 +28,7 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
-}
-
-export default function Post() {
-    return <Layout>...</Layout>;
-}
-
-export async function getStaticPaths() {
-    // Return a list of possible value for id
-}
-
-export async function getStaticProps({ params }) {
-    // Fetch necessary data for the blog post using params.id
 }
 
